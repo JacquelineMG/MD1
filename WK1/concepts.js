019333f16efdef88cef2128048efd65acd9da7db
JacquelineMG <j.gilchrist@outlook.com>
2024-01-09 13:10:45 -0800

    account for empty arrays

--- WK1/concepts.js
+++ WK1/concepts.js
@@ -1,6 +1,9 @@
 // joinList takes an array list of things learned and joins them into a string.
 
 const joinList = function(listArr) {
+  if (listArr.length < 1) {
+    return "";
+  }
   let string = listArr[0];
   for (let i = 1; i < listArr.length - 1; i++) {
     string += `, ${listArr[i]}`;
@@ -16,4 +19,8 @@ console.log(`Today I learned about ${concepts}.`);
 const catList = ["Ronnie", "Pip", "Mr. Orange", "Shadow"];
 const cats = joinList(catList);
 console.log(`The neighbourhood cats are named ${cats}.`);
+
+const emptyList = [];
+const empty = joinList(emptyList);
+console.log(`This list is empty: ${empty}!`);
 
