119ce486431b0be706f226a0fbce914fdf160dbd
JacquelineMG <j.gilchrist@outlook.com>
2024-02-21 10:32:45 -0800

    work on mapEx

---
+++ WK2/functions/mapEx.js
@@ -0,0 +1,7 @@
+// example of map function
+
+const lighthouses = ["Gibraltar Point", "Peggy's Point", "Cove Island", "Discovery Island", "Cape Scott", "Point Clark", "Kincardine"];
+
+const nameLength = lighthouses.map((name) => name.length);
+
+console.log(nameLength);
