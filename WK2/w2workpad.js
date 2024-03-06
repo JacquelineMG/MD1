d1c02b0cb45c8f7cebe9dd6893d4c1c9777edf2e
JacquelineMG <j.gilchrist@outlook.com>
2024-03-06 09:55:30 -0800

    start calculateSalesTax function

--- WK2/w2workpad.js
+++
@@ -1,7 +0,0 @@
-const numbers = [1, 2, 3, 4, 5, 7, 10, 14, 17, 18];
-
-const evens = numbers.filter(num => num % 2 === 0);
-const odds = numbers.filter(num => num % 2 !== 0);
-
-console.log("Subset of even numbers:", evens);
-console.log("Subset of odd numbers:", odds);
