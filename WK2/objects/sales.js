f5970358ca7bcbca9c9088e657040180a44e4ca7
JacquelineMG <j.gilchrist@outlook.com>
2024-03-08 11:37:08 -0800

    implement calculateSalesTax function

--- WK2/objects/sales.js
+++ WK2/objects/sales.js
@@ -1,10 +1,13 @@
+// given sales tax rates by province and company sales data
+// calculateSalesTax calculates the total sales and total tax, grouped by company
+
 const salesTaxRates = {
   AB: 0.05,
   BC: 0.12,
   SK: 0.10
 };
 
-const companySalesData = [
+const companySalesData1 = [
   {
     name: "Telus",
     province: "BC",
@@ -22,13 +25,28 @@ const companySalesData = [
   }
 ];
 
-
 const calculateSalesTax = function(salesData, taxRates) {
   const resultOb = {};
-  
 
+  for (const salesD of salesData) {
+    const salesTotal = salesD.sales.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    const taxTotal = salesTotal * (taxRates[salesD.province]);
+
+    if (resultOb[salesD.name]) {
+      resultOb[salesD.name].totalSales += salesTotal;
+      resultOb[salesD.name].totalTaxes += taxTotal;
+
+    } else {
+      if (!resultOb[salesD.name]) {
+        resultOb[salesD.name] = {
+          totalSales: salesTotal,
+          totalTaxes: taxTotal
+        };
+      }
+    }
+  }
   return resultOb;
 };
 
 
-console.log(calculateSalesTax(companySalesData, salesTaxRates));
+console.log(calculateSalesTax(companySalesData1, salesTaxRates));
