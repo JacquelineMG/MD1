a396e4caf668e70272851b950e7b5cf415b78f00
JacquelineMG <j.gilchrist@outlook.com>
2024-05-03 10:42:13 -0700

    working with functions as object properties

--- WK3/wk3workpad.js
+++ WK3/wk3workpad.js
@@ -1,52 +1,17 @@
-const library = {
-
-  tracks:
-  { t01: { id: "t01",
-    name: "Code Monkey",
-    artist: "Jonathan Coulton",
-    album: "Thing a Week Three" },
-  t02: { id: "t02",
-    name: "Model View Controller",
-    artist: "James Dempsey",
-    album: "WWDC 2003"},
-  t03: { id: "t03",
-    name: "Four Thirty-Three",
-    artist: "John Cage",
-    album: "Woodstock 1952"}
+const person = {
+  firstName: 'Bob',  // <= property containing data
+  lastName:  'Smith', // <= ditto
+  fullName: function() {
+    return `${this.firstName} ${this.lastName}`;
   },
-
-  playlists:
-  { p01: { id: "p01",
-    name: "Coding Music",
-    tracks: ["t01", "t02"]
-  },
-  p02: { id: "p02",
-    name: "Other Playlist",
-    tracks: ["t03"]
-  }
+  greeting: function(dayType) {
+    return `Hello, ${this.fullName()}. I hope you have a ${dayType} day!`;
   }
 };
 
 
-// STRETCH:
-// given a query string string, prints a list of tracks
-// where the name, artist or album contains the query string (case insensitive)
-// tip: use "string".search("tri")
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/search
 
+console.log(`Hello, ${person.fullName()}. How are you today?`);
 
-const printSearchResults = function(data, query) {
-  const tracks = data.tracks;
-  const querySearch = new RegExp(query, "i");
+console.log(person.greeting("silly"));
 
-  for (const t in tracks) {
-    let track = tracks[t];
-
-    if (querySearch.test(track.name) || querySearch.test(track.artist) || querySearch.test(track.album)) {
-      console.log(tracks[t]);
-    }
-  }
-
-};
-
-printSearchResults(library, "three");
