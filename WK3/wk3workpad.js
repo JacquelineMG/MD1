3123ef703f0f2b4953d74d5578f4f43f234a86e2
JacquelineMG <j.gilchrist@outlook.com>
2024-05-05 10:52:02 -0700

    playing with methods

--- WK3/wk3workpad.js
+++ WK3/wk3workpad.js
@@ -1,13 +1,18 @@
-const myfunc = function() {
-  return "Good morning!";
+let group = {
+  title: "Our Group",
+  students: ["Albert", "Betty", "Carl", "Dinah"],
+  showList() {
+    let studentList = "";
+
+    for (const s of this.students) {
+      if (studentList.length) {
+        studentList += `, ${s}`;
+      } else {
+        studentList += s;
+      }
+    }
+    return `Currently Enrolled Students: ${studentList}`;
+  }
 };
 
-const otherfunc = myfunc();
-
-const obj = {
-  hello: "hello",
-  goodbye: "goodbye",
-  greeting: otherfunc
-};
-
-console.log(obj.greeting);
+console.log(group.showList());
