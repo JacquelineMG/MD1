679ceb34648e68cc17fc5bdb35eb6428f518bd3d
JacquelineMG <j.gilchrist@outlook.com>
2024-05-10 15:34:35 -0700

    start multiplicationTable function

---
+++ Prep/Katas/10_MultiplicationTable.js
@@ -0,0 +1,10 @@
+// generate a multiplication table for the values from 1 to the provided number
+
+const multiplicationTable = (maxValue) => {
+    
+
+};
+
+console.log(multiplicationTable(1));
+console.log(multiplicationTable(5));
+console.log(multiplicationTable(10));
