7f0ebe07eadb84d0d3efb33c39336a2851e1d649
JacquelineMG <j.gilchrist@outlook.com>
2024-05-10 15:59:59 -0700

    implement multiplicationTable function

--- Prep/Katas/10_MultiplicationTable.js
+++ Prep/Katas/10_MultiplicationTable.js
@@ -1,10 +1,20 @@
 // generate a multiplication table for the values from 1 to the provided number
 
 const multiplicationTable = (maxValue) => {
-    
+  let row = "";
+  let multiple = 1;
 
+  while (multiple <= maxValue) {
+    for (let i = 1; i <= maxValue; i++) {
+      row += (i * multiple) + " ";
+    }
+    row += "\n";
+    multiple ++;
+  } return row;
 };
 
 console.log(multiplicationTable(1));
+console.log("-----");
 console.log(multiplicationTable(5));
+console.log("-----");
 console.log(multiplicationTable(10));
