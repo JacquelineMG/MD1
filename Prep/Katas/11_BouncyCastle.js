3e8111d699d01ec5e7df4731e3092733215c7dae
JacquelineMG <j.gilchrist@outlook.com>
2024-05-16 10:13:40 -0700

    implement coneVolume()

--- Prep/Katas/11_BouncyCastle.js
+++ Prep/Katas/11_BouncyCastle.js
@@ -3,3 +3,11 @@
 const sphereVolume = function(radius) {
   return (4 / 3) * Math.PI * (radius ** 3);
 };
+
+
+
+// coneVolume() will calculate the volume of a cone given a radius and a height
+
+const coneVolume = function(radius, height) {
+  return (1 / 3) * Math.PI * (radius ** 2) * height;
+};
