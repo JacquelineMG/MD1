823041ae810dd0ff111c90f309b84a9b8464df51
JacquelineMG <j.gilchrist@outlook.com>
2024-05-16 10:17:44 -0700

    implement prismVolume()

--- Prep/Katas/11_BouncyCastle.js
+++ Prep/Katas/11_BouncyCastle.js
@@ -1,13 +1,16 @@
 // sphereVolume()will calculate the volume of a sphere given a radius
-
 const sphereVolume = function(radius) {
   return (4 / 3) * Math.PI * (radius ** 3);
 };
 
 
-
 // coneVolume() will calculate the volume of a cone given a radius and a height
-
 const coneVolume = function(radius, height) {
   return (1 / 3) * Math.PI * (radius ** 2) * height;
+};
+
+
+// prismVolume() will calculate the volume of a prism given a height, a width, and a depth
+const prismVolume = function(height, width, depth) {
+  return (width * depth) * height;
 };
