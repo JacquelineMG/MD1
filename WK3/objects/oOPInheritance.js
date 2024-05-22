53c8efc6e5feb6cd26259b8c7d003c3897edb60d
JacquelineMG <j.gilchrist@outlook.com>
2024-05-22 08:56:24 -0700

    add method to Person class

--- WK3/objects/oOPInheritance.js
+++ WK3/objects/oOPInheritance.js
@@ -1,23 +1,31 @@
-class Student {
+class Person {
+
   constructor(name, quirkyFact) {
     this.name = name;
     this.quirkyFact = quirkyFact;
-  }
-
-  enroll(cohort) {
-    this.cohort = cohort;
   }
 
   bio() {
     return `My name is ${this.name} and my quirky fact is: ${this.quirkyFact}`;
   }
+
+  attendingConference(status, role) {
+    this.attendingConference = status;
+    if (this.attendingConference === true) {
+      this.conferenceRole = role;
+    }
+  }
 }
 
-class Mentor {
-  constructor(name, quirkyFact) {
-    this.name = name;
-    this.quirkyFact = quirkyFact;
+class Student extends Person {
+
+  enroll(cohort) {
+    this.cohort = cohort;
   }
+}
+
+
+class Mentor extends Person {
 
   goOnShift() {
     this.onShift = true;
@@ -26,8 +34,26 @@ class Mentor {
   goOffShift() {
     this.onShift = false;
   }
-
-  bio() {
-    return `My name is ${this.name} and my quirky fact is: ${this.quirkyFact}`;
-  }
 }
+
+
+const student1 = new Student("Ariel", "I like cold water swimming.");
+student1.enroll(2025);
+
+student1.attendingConference(false, "guest");
+
+console.log(student1);
+
+console.log(student1.bio());
+
+
+console.log("----------------------------------------------------");
+console.log("----------------------------------------------------");
+
+const mentor1 = new Mentor("Ursula", "I have pet eels.");
+
+console.log(mentor1.bio());
+
+mentor1.attendingConference(true, "speaker");
+
+console.log(mentor1);
