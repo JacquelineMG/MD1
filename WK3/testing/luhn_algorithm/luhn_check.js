967c2a6c9fe2729344025f077cbeb43365578cae
JacquelineMG <j.gilchrist@outlook.com>
2024-05-23 10:45:41 -0700

    start check()

--- WK3/testing/luhn_algorithm/luhn_check.js
+++ WK3/testing/luhn_algorithm/luhn_check.js
@@ -1,9 +1,12 @@
 // check() returns whether a given number is valid or not according to the Luhn Algorithm
 
 const check = (number) => {
+  const checkNum = Number(number.toString().slice(-1));
+  const payloadArr = number.toString().split("").reverse().slice(1);
 
-  
-  return false;
+  console.log(payloadArr);
 };
+
+console.log(check(123457));
 
 module.exports = check;
