81b23525fe787a23f01c2b80a9f90dd83a77576b
JacquelineMG <j.gilchrist@outlook.com>
2024-05-28 10:21:16 -0700

    start popBottles activity

--- WK3/stretchProblems/popBottlesNP.js
+++ WK3/stretchProblems/popBottlesNP.js
@@ -1,16 +1,11 @@
 const calculateBottles = (initialBottles) => {
-  console.log(`initialBottles: ${initialBottles}`);
-  if (initialBottles < 1) {
-    return initialBottles;
-  }
+
   const bottlesFromCaps = Math.floor(initialBottles / 4);
   const bottlesFromReturns = Math.floor(initialBottles / 2);
-  const extra = initialBottles - bottlesFromReturns;
-  console.log("extra:", initialBottles, "-", bottlesFromReturns, "=", extra);
   const newBottles = bottlesFromCaps + bottlesFromReturns;
 
-  return initialBottles + calculateBottles(newBottles);
+  return initialBottles + newBottles;
 
 };
 
-console.log(calculateBottles(10));
+console.log(calculateBottles(5));
