ee44efccc03912db39f0f399b21864236624060d
JacquelineMG <j.gilchrist@outlook.com>
2024-05-29 10:39:48 -0700

    work on popBottles activity

--- WK3/stretchProblems/popBottles.js
+++ WK3/stretchProblems/popBottles.js
@@ -3,25 +3,8 @@
 // Each bottle of pop costs $2 to purchase
 
 
-const purchaseBottles = (money) => {
-  return Math.floor(money / 2);
-};
-
-const calculateBottles = (initialBottles) => {
-
-  const bottlesFromCaps = Math.floor(initialBottles / 4);
-  const bottlesFromReturns = Math.floor(initialBottles / 2);
-  const totalBottles = initialBottles + bottlesFromCaps + bottlesFromReturns;
-
-  let summary = {
-    totalBottles: totalBottles,
-    bottlesPurchased: initialBottles,
-    bottlesFromCaps: bottlesFromCaps,
-    bottlesFromReturns: bottlesFromReturns
-  };
-
-  return summary;
+const calculateBottles = (money, bottles, summary) => {
 
 };
 
-console.log(calculateBottles(purchaseBottles(15)));
+console.log(calculateBottles(15));
--- WK3/stretchProblems/popBottlesNP.js
+++ WK3/stretchProblems/popBottlesNP.js
@@ -1,11 +1,31 @@
+
+const purchaseBottles = (money) => {
+  return Math.floor(money / 2);
+};
+
 const calculateBottles = (initialBottles) => {
 
-  const bottlesFromCaps = Math.floor(initialBottles / 4);
-  const bottlesFromReturns = Math.floor(initialBottles / 2);
-  const newBottles = bottlesFromCaps + bottlesFromReturns;
+  if (initialBottles === 1) {
+    return initialBottles;
+  }
 
-  return initialBottles + newBottles;
+  const bottlesFromCaps = Math.floor(initialBottles / 4);
+  const remainingCaps = initialBottles - (bottlesFromCaps * 4);
+
+  console.log("caps", remainingCaps);
+
+
+  const bottlesFromReturns = Math.floor(initialBottles / 2);
+  const remainingBottles = initialBottles - (bottlesFromReturns * 2);
+
+  const newBottles = bottlesFromCaps + bottlesFromReturns;
+  const currentBottles = newBottles + remainingBottles;
+
+  return initialBottles + calculateBottles(currentBottles);
 
 };
 
-console.log(calculateBottles(5));
+console.log(calculateBottles(purchaseBottles(40)));
+
+
+
