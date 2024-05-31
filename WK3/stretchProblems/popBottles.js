48cbe531ce505de928dbb9e2ab81aa304ddb750d
JacquelineMG <j.gilchrist@outlook.com>
2024-05-31 11:25:51 -0700

    work on popBottles activity

--- WK3/stretchProblems/popBottles.js
+++ WK3/stretchProblems/popBottles.js
@@ -6,16 +6,13 @@
 const calculateBottles = (money, bottles, summary) => {
   summary = (summary || {
     
-    money:   {invested: 0, available: 0},
-    bottles: {bought: 0},
-    empties: {earned: 0,   remaining: 0,  total: 0},
-    caps:    {earned: 0,   remaining: 0,  total: 0}
+    money:   {invested: 0,  remaining: 0},
+    bottles: {bought: 0,   earned: 0,   remaining: 0},
+    caps:    {remaining: 0}
 
   });
 
-  let currentBottles = 0;
-
-  const currentMoney = money + summary.money.available;
+  const currentMoney = money + summary.money.remaining;
 
   if (money) {
     summary.money.invested += money;
@@ -23,14 +20,28 @@ const calculateBottles = (money, bottles, summary) => {
   
   if (currentMoney > 1) {
     summary.bottles.bought = Math.floor((currentMoney) / 2);
-    currentBottles += summary.bottles.bought;
-    summary.money.available = currentMoney % 2;
+    summary.bottles.remaining += summary.bottles.bought;
+    summary.money.remaining = currentMoney % 2;
   }
 
-  
+  if (summary.bottles.remaining > 1) {
+    // redeem caps
+    summary.bottles.earned += Math.floor(summary.bottles.remaining / 4);
+    summary.caps.remaining += summary.bottles.remaining % 4;
+
+    
+    // redeem bottles
+    summary.bottles.earned += Math.floor(summary.bottles.remaining / 2);
+    summary.bottles.remaining = summary.bottles.remaining % 2;
+  }
+
+  if (summary.caps.remaining > 3) {
+    summary.bottles.earned += Math.floor(summary.bottles.remaining / 4);
+    summary.caps.remaining += summary.bottles.remaining % 4;
+  }
 
 
   return summary;
 };
 
-console.log(calculateBottles(4));
+console.log(calculateBottles(10));
