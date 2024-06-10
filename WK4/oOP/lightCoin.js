b0e77c06450b6d4dbcccdfb82138c6c2c42b34ea
JacquelineMG <j.gilchrist@outlook.com>
2024-06-10 12:46:00 -0700

    work on lightCoin exercise

---
+++ WK4/oOP/lightCoin.js
@@ -0,0 +1,57 @@
+
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+    this.balance = 0;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+}
+
+class Transaction {
+  constructor(account, amount) {
+    this.amount = amount;
+    this.account = account;
+  }
+
+  commit() {
+    this.account.balance += this.value;
+  }
+
+}
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount;
+  }
+}
+
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount;
+  }
+}
+
+// DRIVER CODE BELOW
+// We use the code below to "drive" the application logic above and make sure it's working as expected
+
+const myAccount = new Account("MoneyBags");
+
+const t1 = new Withdrawal(myAccount, 50.25);
+t1.commit();
+console.log('Transaction 1:', t1);
+
+const t2 = new Withdrawal(myAccount, 9.99);
+t2.commit();
+console.log('Transaction 2:', t2);
+
+const t3 = new Deposit(myAccount, 120.00);
+t3.commit();
+console.log('Transaction 3:', t3);
+
+console.log('Balance:', myAccount.balance);
