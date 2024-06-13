a5d8fc10d2e3d8b62eed9c6e728953b78b746978
JacquelineMG <j.gilchrist@outlook.com>
2024-06-13 10:18:54 -0700

    work through OOP best practices reading

--- WK4/oOP/bestPracticesEx.js
+++ WK4/oOP/bestPracticesEx.js
@@ -1,3 +1,45 @@
+class TaskList {
+  constructor() {
+    this.list = [];
+  }
+
+  addNewTask(taskName) {
+    this.list.push({
+      taskName: taskName,
+      completed: false
+    });
+  }
+
+  completeTask(taskName) {
+    for (let t of this.list) {
+      if (t.taskName === taskName) {
+        t.completed = true;
+      }
+    }
+  }
+
+  removeTask(taskName) {
+    this.list = this.list.filter(task => task.taskName !== taskName);
+  }
+}
+
+const myTask = new TaskList();
+
+myTask.addNewTask("wash socks");
+myTask.addNewTask("wash dishes");
+myTask.addNewTask("read book");
+myTask.addNewTask("make food");
+myTask.addNewTask("wash dishes");
+
+console.log(myTask);
+
+myTask.completeTask("wash socks");
+myTask.removeTask("wash dishes");
+console.log(myTask);
+
+
+////////////////////////////////////////
+/*
 class Pizza {
   constructor() {
     this.toppings = [];
@@ -23,4 +65,4 @@ console.log(myPizza.addTopping("spinach"));
 console.log(myPizza.addTopping("mushrooms"));
 
 console.log(myPizza);
-
+*/
