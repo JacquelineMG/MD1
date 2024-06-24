329456980e2f641f8f2ef9386fbc93bc2160ae71
JacquelineMG <j.gilchrist@outlook.com>
2024-06-24 09:23:59 -0700

    work through setTimeout Practice

---
+++ WK4/async/hello_timeout.js
@@ -0,0 +1,26 @@
+// Challenge #2
+// Instead of printing the entire sentence together, have our code print out each of the three words one at a time, 1 second apart.
+
+setTimeout(() => {
+  console.log("4311o");
+}, 1000);
+
+setTimeout(() => {
+  console.log("th3r3");
+}, 2000);
+
+setTimeout(() => {
+  console.log("w0rld");
+}, 3000);
+
+
+// Challenge #1
+// Have our file print out these three words 4311o th3r3 w0r1d three seconds after the file is run
+
+/*
+
+setTimeout(() => {
+  console.log("4311o th3r3 w0r1d");
+}, 3000);
+
+*/
