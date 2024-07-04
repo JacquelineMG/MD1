ac188e25c0a69a666768330f6a55b42ee4acd83e
JacquelineMG <j.gilchrist@outlook.com>
2024-07-04 10:11:54 -0700

    implement profileGen()

--- WK4/async/profileGen.js
+++ WK4/async/profileGen.js
@@ -5,25 +5,55 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-const userInfo = {};
+const questions = [
+  "What's your name? Nicknames are also acceptable :) ",
+  "What's an activity you like doing? ",
+  "What do you listen to while doing that? ",
+  "Which meal is your favourite (eg: dinner, brunch, etc.) ",
+  "What's your favourite thing to eat for that meal? ",
+  "Which sport is your absolute favourite? ",
+  "What is your superpower? In a few words, tell us what you are amazing at! "
+];
 
+const responses = [
+  "Nice to meet you! \n",
+  "I've never tried that before. \n",
+  "Jazzy! \n",
+  "I like brunch. \n",
+  "The food of champions! \n",
+  "I excel at sitting still. \n",
+  "Thanks for sharing! \n"
+];
 
-rl.question("What's your name? Nicknames are also acceptable :) ", (answer) => {
-  userInfo.name = answer;
-  console.log(`Nice to meet you, ${answer}!`);
+const userInfo = {
+  0: "",
+  1: "",
+  2: "",
+  3: "",
+  4: "",
+  5: "",
+  6: ""
+};
 
-  rl.question("What's an activity you like doing? ", (answer) => {
-    userInfo.activity = answer;
-    console.log(`I've never tried ${answer} before.`);
+const profileGen = (index) => {
 
-    rl.question("What do you listen to while doing that? ", (answer) => {
-      userInfo.music = answer;
-      console.log(`Jazzy.`);
+  if (index === 7) {
+    console.log(`\n Here's your profile:
+
+      ${userInfo[0]} loves listening to ${userInfo[2]} while ${userInfo[1]};
+      devouring ${userInfo[4]} for ${userInfo[3]}; prefers ${userInfo[5]} over any other sport;
+      and is amazing at ${userInfo[6]}.
+`
+    );
+    rl.close();
+  } else {
+    rl.question(questions[index], (answer) => {
+      userInfo[index] = answer;
+      console.log(responses[index]);
+      profileGen(index + 1);
     });
+  
+  }
+};
 
-  });
-
-});
-
-
-
+profileGen(0);
