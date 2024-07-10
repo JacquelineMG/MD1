e538f5d8f4b39cd3d9fb7b012ea550e91acdc5d8
JacquelineMG <j.gilchrist@outlook.com>
2024-07-10 09:20:08 -0700

    set up transpose file

---
+++ WK4/transpose.js
@@ -0,0 +1,37 @@
+const transpose = function(matrix) {
+  // Replace this code with your solution
+  return matrix;
+};
+
+// Do not edit this function.
+const printMatrix = (matrix) => {
+  for (const row of matrix) {
+    for (const el of row) {
+      process.stdout.write(el + " ");
+    }
+    process.stdout.write("\n");
+  }
+};
+
+printMatrix(
+  transpose([
+    [1, 2, 3, 4],
+    [1, 2, 3, 4],
+    [1, 2, 3, 4],
+    [1, 2, 3, 4],
+  ])
+);
+
+console.log("----");
+
+printMatrix(
+  transpose([
+    [1, 2],
+    [3, 4],
+    [5, 6],
+  ])
+);
+
+console.log("----");
+
+printMatrix(transpose([[1, 2, 3, 4, 5, 6, 7]]));
